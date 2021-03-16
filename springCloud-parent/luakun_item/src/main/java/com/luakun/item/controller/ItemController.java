31282e1fed76290950a83c34ce31fcf22cb54ecf
luakun <13006605495@163.com>
2021-03-16 19:43:57 +0800

    SpringCloud's gatway second commit

--- springCloud-parent/luakun_item/src/main/java/com/luakun/item/controller/ItemController.java
+++ springCloud-parent/luakun_item/src/main/java/com/luakun/item/controller/ItemController.java
@@ -1,10 +1,7 @@
 package com.luakun.item.controller;
 
 import com.luakun.item.pojo.OrderInfo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,7 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
 
     @GetMapping("{itemId}")
-    public OrderInfo getInfo(@PathVariable(name = "itemId") String itemId){
+    public OrderInfo getInfo(@PathVariable(name = "itemId") String itemId, @RequestHeader(value = "name" ) String value){
+        //获取头信息 网关
+        System.out.println("头信息:" +value);
         System.out.println("是我啊 我是 1 号 我先开始啦~~~~");
         OrderInfo orderInfo = new OrderInfo();
         orderInfo.setItemId(itemId);
